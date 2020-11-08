72df7716e27446950216e0eecf4d5eae3427a7c2
djm160830 <djm160830@utdallas.edu>
2020-11-08 13:16:20 -0600

    Add NER functionality using Stanford NER Tagger

--- hw5.py
+++ hw5.py
@@ -4,26 +4,66 @@
 # Assignment 5
 
 import twitter
-import pdb
+import os
+from dotenv import load_dotenv
+import nltk
+
+# DELETE IF USING SPACY
+# from nltk.tag import StanfordNERTagger
+# from nltk.tokenize import word_tokenize
+
+# Debugging
 import inspect
-# import os
-# from dotenv import load_dotenv
+import pdb
+
+# DELETE IF USING SPACY
+# Set java.exe location
+# os.environ['JAVAHOME'] = "C:\\Program Files\\Java\\jdk-13.0.1\\bin\\java.exe"
+
+project_folder = os.getcwd()  # Get current directory
+load_dotenv(os.path.join(project_folder, '.env')) # Read from .env
+
+# Twitter API
+API_KEY=os.getenv('API_KEY')
+API_KEY_SECRET=os.getenv('API_KEY_SECRET')
+ACCESS_TOKEN=os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET=os.getenv('ACCESS_TOKEN_SECRET')
+# Stream filter parameters
+USERS=['@twitter']
+LANGUAGES=['en']	
+# Stanford NER Tagger
+CLASSIFIER=os.path.join(project_folder, 'stanford-ner-4.0.0\\stanford-ner-4.0.0\\classifiers\\english.all.3class.distsim.crf.ser.gz')
+JAR_FILE=os.path.join(project_folder, 'stanford-ner-4.0.0\\stanford-ner-4.0.0\\stanford-ner.jar')
+
 
 def listener():
-	pdb.set_trace()
-	
-# 	load_dotenv()
-# # Client keys
-# # Spotify
-# CLIENT_ID = os.getenv('CLIENT_ID')
-# CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-# # Genius
-# GENIUS_CLIENT_ID = os.getenv('GENIUS_CLIENT_ID')
-# GENIUS_CLIENT_SECRET = os.getenv('GENIUS_CLIENT_SECRET')
-# GENIUS_CLIENT_ACCESS_TOKEN = os.getenv('GENIUS_CLIENT_ACCESS_TOKEN')
-
-
+	return twitter.Api(
+		consumer_key=API_KEY,
+		consumer_secret=API_KEY_SECRET,
+		access_token_key=ACCESS_TOKEN,
+		access_token_secret=ACCESS_TOKEN_SECRET)
 
 if __name__ == "__main__":
 	# Set up a listener
-	listener()
+	api = listener()
+
+	# Get a filtered view of public statuses
+	# NOTE: check if there is a line['extended_tweet']['full_text'] before line['text']
+	for i, line in enumerate(api.GetStreamFilter(track=USERS, languages=LANGUAGES)):
+		if i==0: break;
+
+	# DELETE IF USING SPACY
+	# # TODO 2: Set up a Named Entity Recognition module to perform a real time count of entities
+	# st = StanfordNERTagger(
+	# 	CLASSIFIER,
+	# 	JAR_FILE,
+	# 	encoding='utf-8')
+
+	# # INSPECT st.tag -> <Signature (self, tokens)>
+
+	pdb.set_trace()
+
+
+
+
+# TRY USING SPACY
