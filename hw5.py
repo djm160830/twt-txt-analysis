d2e5bccf49c8f9b6a68116161f01802f620e9850
djm160830 <djm160830@utdallas.edu>
2020-11-07 23:57:41 -0600

    Add first instance of source code

--- hw5.py
+++ hw5.py
@@ -3,4 +3,27 @@
 # SECTION:		CS4372.001
 # Assignment 5
 
+import twitter
+import pdb
+import inspect
+# import os
+# from dotenv import load_dotenv
+
+def listener():
+	pdb.set_trace()
+	
+# 	load_dotenv()
+# # Client keys
+# # Spotify
+# CLIENT_ID = os.getenv('CLIENT_ID')
+# CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+# # Genius
+# GENIUS_CLIENT_ID = os.getenv('GENIUS_CLIENT_ID')
+# GENIUS_CLIENT_SECRET = os.getenv('GENIUS_CLIENT_SECRET')
+# GENIUS_CLIENT_ACCESS_TOKEN = os.getenv('GENIUS_CLIENT_ACCESS_TOKEN')
+
+
+
 if __name__ == "__main__":
+	# Set up a listener
+	listener()
