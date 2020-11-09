6190f61bc4de71f1ddd1c88db29ee9d34238efce
djm160830 <djm160830@utdallas.edu>
2020-11-09 01:24:50 -0600

    Add location filter & NER w/ spacy

--- hw5.py
+++ hw5.py
@@ -3,22 +3,28 @@
 # SECTION:		CS4372.001
 # Assignment 5
 
+import pdb
 import twitter
 import os
 from dotenv import load_dotenv
-import nltk
 
 # DELETE IF USING SPACY
+# import nltk
 # from nltk.tag import StanfordNERTagger
-# from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize
+import re
+
+import spacy
+# import en_core_web_sm
 
 # Debugging
 import inspect
-import pdb
 
 # DELETE IF USING SPACY
 # Set java.exe location
 # os.environ['JAVAHOME'] = "C:\\Program Files\\Java\\jdk-13.0.1\\bin\\java.exe"
+nlp = spacy.load("en_core_web_sm")
+# nlp = en_core_web_sm.load()
 
 project_folder = os.getcwd()  # Get current directory
 load_dotenv(os.path.join(project_folder, '.env')) # Read from .env
@@ -29,7 +35,16 @@ API_KEY_SECRET=os.getenv('API_KEY_SECRET')
 ACCESS_TOKEN=os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET=os.getenv('ACCESS_TOKEN_SECRET')
 # Stream filter parameters
-USERS=['@twitter']
+# USERS=['@twitter']
+# COORDINATES='32.77,-96.79'
+# COORDINATES=["-122.75,36.8,-121.75,37.8,-74,40,-73,41"]
+# COORDINATES=["-98.49243164062501,29.38217507514529,-96.80053710937501,32.8149783969858"] # long-lat,long-lat
+COORDINATES=["-97.94311523437501,32.31499127724556,-96.470947265625,33.04550781490999,\
+				-95.71289062500001,29.46829664171322,-95.05371093750001,30.050076521698735,\
+				-118.56445312500001,33.58716733904659,-117.69653320312501,34.243594729697406,\
+				-121.88232421875001,38.19502155795575,-121.11328125000001,38.90813299596705,\
+				-122.87109375000001,47.18971246448421,-121.88232421875001,47.65058757118736,\
+				-74.54772949218751,40.35282369083777,-72.97668457031251,41.529141988723104"]
 LANGUAGES=['en']	
 # Stanford NER Tagger
 CLASSIFIER=os.path.join(project_folder, 'stanford-ner-4.0.0\\stanford-ner-4.0.0\\classifiers\\english.all.3class.distsim.crf.ser.gz')
@@ -49,8 +64,23 @@ if __name__ == "__main__":
 
 	# Get a filtered view of public statuses
 	# NOTE: check if there is a line['extended_tweet']['full_text'] before line['text']
-	for i, line in enumerate(api.GetStreamFilter(track=USERS, languages=LANGUAGES)):
-		if i==0: break;
+	for i, line in enumerate(api.GetStreamFilter(locations=COORDINATES, languages=LANGUAGES)): # I REMOVED track=USERS
+		if i==5: break
+		else:
+			txt = line['extended_tweet']['full_text'] if 'extended_tweet' in line else line['text']
+			# Set up doc
+			txt = txt.lower()
+			# COPY EVERYTHING BELOW INTO HERE, FIGURE OUT A WAY TO PRINT COUNTS AND STUFF IN REAL TIME
+			pdb.set_trace()
+			# DON'T RUN ANYTHIG YET FINISH OPERATIONS IN HERE
+
+
+	# txt=line['text']
+	# tt=txt.lower()
+	# ttt=re.sub("(@(\w){1,15}(?!\w))","",tt)
+	# tttt=re.sub("[^a-z0-9\s]","",ttt)
+	# doc=nlp(tttt)
+	# [(X.text,X.label_) for X in doc.ents]
 
 	# DELETE IF USING SPACY
 	# # TODO 2: Set up a Named Entity Recognition module to perform a real time count of entities
